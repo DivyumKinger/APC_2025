49ddc8b5556f9e07c957c1741f6d7bee6edcb185
Divyum Kinger <divyumkinger2005@gmail.com>
2025-07-10 15:16:32 +0530

    added O2

--- July_10_2025/O2/Main.java
+++ July_10_2025/O2/Main.java
@@ -1,7 +1,34 @@
 package July_10_2025.O2;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
+@SuppressWarnings("CallToPrintStackTrace")
 public class Main {
 	public static void main(String[] args) {
-//		String filePath
+		String file = "src/July_10_2025/O2/input.txt";
+		String dest = "src/July_10_2025/O2/output.txt";
+		
+		FileReader fr = null;
+		FileWriter fw = null;
+		
+		try {
+			fr = new FileReader(file);
+			fw = new FileWriter(dest);
+			int c;
+			while ((c = fr.read()) != -1) {
+				fw.write(c);
+			}
+			System.out.println("Text file copied successfully.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fr != null) fr.close();
+				if (fw != null) fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
